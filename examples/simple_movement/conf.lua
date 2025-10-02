03071a8752e9c23d586665bee89ad6be94703b5c
ApetorSkol <matej.slivka33@proton.me>
2025-10-02 19:04:57 +0400

    adding config

---
+++ examples/simple_movement/conf.lua
@@ -0,0 +1,9 @@
+--[[
+    This is configuration file for game setup
+]]--
+function love.conf(t)
+    t.window.title = "Runner Game"
+    t.window.borderless = false
+    t.window.resizable = true
+    --t.window.fullscreentype = "exclusive"
+end
